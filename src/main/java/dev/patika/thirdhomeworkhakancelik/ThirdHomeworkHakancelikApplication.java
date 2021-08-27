6cb39530c30f1bb47e3bf47e1310ea1aa0960306
celikhakan41@yahoo.com <xsamuray41@gmail.com>
2021-08-28 02:00:45 +0300

    First commit

---
+++ src/main/java/dev/patika/thirdhomeworkhakancelik/ThirdHomeworkHakancelikApplication.java
@@ -0,0 +1,13 @@
+package dev.patika.thirdhomeworkhakancelik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdHomeworkHakancelikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdHomeworkHakancelikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/patika/thirdhomeworkhakancelik/ThirdHomeworkHakancelikApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.thirdhomeworkhakancelik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdHomeworkHakancelikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
